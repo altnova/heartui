d0b7e747aff74687387125e3454085478da60692
altnova <arina@lateralsummer.com>
2018-10-04 19:32:40 +0300

    [~]

--- src/main.c
+++ src/main.c
@@ -11,31 +11,11 @@
 #include "dat.h"
 
 
-
-	/*
-	C cap[6] = 			 ".:::.\0";
-	C sec_line[12] = 	":::::::.:::\0";
-	C th_line[16] = 	":::::::::::::::\0";
-	C four_line_1[6] = 	"\'::::\0";
-	C four_line_2[6] =		"::::\'\0";
-	C five_line[12] = 		"\':::::::::\'\0";
-	C six_line[8] =		"\':::::\'\0";
-	C sev_line[4] = 		"\':\'\0";*/
-
-	// C bck_chr[3] = " :\0";
-
-
 I main()
 {
 	srand(time(NULL));
-
-	// O("Im here!\n");
-	if (access("../bin/db.dat", F_OK) == -1) 
+	if (access("../bin/db.dat", F_OK) == -1 || access("../bin/idx.dat", F_OK) == -1) 
 		make_db();
-
-	// O("Im here!\n");
-
 	draw_heart();
-
 	R0;
 }
