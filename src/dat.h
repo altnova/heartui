cd99c82de04146cbcc238d7e223bced58020b3bf
altnova <arina@lateralsummer.com>
2018-10-04 19:29:45 +0300

    [+] get dif structs

--- src/dat.h
+++ src/dat.h
@@ -1,4 +1,8 @@
 
+
 ext I get_addr(I num, FILE *idx);
 ext V make_db();
 ext S* get_heart(FILE* db, I ptr, S* heart);
+ext V get_background(FILE *db, background back, I ptr);
+ext V get_params(FILE *idx, params par);
+
