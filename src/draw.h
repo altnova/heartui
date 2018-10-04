5750fd6a387599b023e5798900542b477e477ebd
altnova <arina@lateralsummer.com>
2018-10-05 01:26:15 +0300

    [~]

--- src/draw.h
+++ src/draw.h
@@ -1,9 +1,3 @@
-
-
-
-
-
 ext V clear_str(S str, I j);
 ext V draw_heart();
-// ext I fill_heart(FILE *ptr, S background_colour, C background_char, S heart_colour, S blink, S blink_colour, S string, S string_colour);
 ext V include_string(S str_1, S str_2, I ptr, I max);
