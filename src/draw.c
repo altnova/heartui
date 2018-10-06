998181cf65c7442885e782b957823f6e2f992801
altnova <arina@lateralsummer.com>
2018-10-06 23:11:17 +0300

    [+] draw_side(); now passing background par between functions

--- src/draw.c
+++ src/draw.c
@@ -13,44 +13,22 @@
 #include <pwd.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include "rows.h"
 
-V show_img(S* heart, S background_col, C background_char, ter_conf _ter_conf);
-
-V window_dif_change(S* rows, S col, C ch, ter_conf _ter_conf, I pause)
+V draw_side(S col, C ch, I w, I dif)
 {
-	struct winsize a;
-	ter_conf _ter_var = &a;
-	I j, i, p = SEC/60;
-	j = pause/p;
-
-	show_img(rows, col, ch, _ter_conf);
-
-	for (i = 0; i < j; i++) {
-		usleep(p);
-		ioctl(STDOUT_FILENO, TIOCGWINSZ, _ter_var);
-		if (_ter_var->ws_col != _ter_conf->ws_col || _ter_var->ws_row != _ter_conf->ws_row)	
-			show_img(rows, col, ch, _ter_conf);
-	}
+	O("%s", col);
+	DO((w - HEART_W)/2 + dif, {O("%c", ch);});
+	fflush(stdout);
 }
 
-V include_string(S str_1, S str_2, I ptr, I max)				//<	writes in str_2 into str_1
-{
-	I i;
-	for (i = 0; str_2[i] != 0 && i < max; i++) 
-		str_1[ptr + i] = str_2[i];		
-}
 
-V clear_str(S str, I j)											//<	set 0 for str[0] -->str[j-1]
-{
-	for (I i = 0; i < j; i++)
-		str[i] = 0;
-}
-
-V show_img(S* heart, S background_col, C background_char, ter_conf _ter_conf)								//< print img 
+V show_img(background heart_par, ter_conf _ter_conf)								//< print img 
 {
 	I j, dif, k = 0;
 	struct winsize a;
 	ter_conf _ter_var = &a;
+	C cnrm[7] = "\x1b[0;0m";
 
 	gotoxy(0, 0);
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, _ter_conf);															//<	get window size
@@ -60,36 +38,92 @@ V show_img(S* heart, S background_col, C background_char, ter_conf _ter_conf)			
 		R;
 	}
 
-	O("%s", background_col);																				//<	set background colour
-	DO((_ter_conf->ws_row - HEART_H)/2, {DO(_ter_conf->ws_col, {O("%c", background_char);}); O("\n");}); 	//< draw top
+	O("%s", heart_par->bck_col);																			//<	set background colour
+	DO((_ter_conf->ws_row - HEART_H)/2, {DO(_ter_conf->ws_col, {O("%c", heart_par->ch);}); O("\n");}); 		//< draw top
 	fflush(stdout);
 
-	for (j = 0; j < HEART_H; j++){																			//<	for each img line
-		O("%s", background_col);
-		DO((_ter_conf->ws_col - HEART_W)/2, {O("%c", background_char);});									//<	draw left
-		O("%s", heart[j]);																					//<	img line
-		dif = (_ter_conf->ws_col % 2) ? 0 : 1;
-		O("%s", background_col);
-		DO((_ter_conf->ws_col - HEART_W)/2 + dif, {O("%c", background_char);});								//<	draw right
-		fflush(stdout);
-		O("\n");
-	}
+	dif = (_ter_conf->ws_col % 2) ? 1 : 0;
+	dif = 1;
+	/*	row_1()		*/
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, 0);								//<	draw left
+	row_1(heart_par->bck_col, heart_par->ch, heart_par->hrt_col, cnrm);
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, dif);
+	O("\n");
+
+	/*	row_2()		*/
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, 0);
+	row_2(heart_par->hrt_col, heart_par->blink, heart_par->bln_col, cnrm);
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, dif);
+	O("\n");
+
+	/*	row_3()	*/
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, 0);
+	row_3(heart_par->hrt_col, cnrm);
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, dif);
+	O("\n");
+
+	/* row_4()	*/
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, 0);
+	row_4(heart_par->bck_col, heart_par->ch, heart_par->hrt_col, heart_par->string, heart_par->str_col, cnrm);
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, dif);
+	O("\n");
+
+	/*	row_5()	*/
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, 0);
+	row_5(heart_par->bck_col, heart_par->ch, heart_par->hrt_col, cnrm);
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, dif);
+	O("\n");
+
+	/*	row_6()	*/
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, 0);
+	row_6(heart_par->bck_col, heart_par->ch, heart_par->hrt_col, cnrm);
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, dif);
+	O("\n");
+
+	/*	row_7()	*/
+
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, 0);
+	row_7(heart_par->bck_col, heart_par->ch, heart_par->hrt_col, cnrm);
+	draw_side(heart_par->bck_col, heart_par->ch, _ter_conf->ws_col, dif);
+	O("\n");
+
 
 	dif = (_ter_conf->ws_row % 2) ? 1 : 0;
-	DO((_ter_conf->ws_row - HEART_H)/2 + dif, {DO(_ter_conf->ws_col, {O("%c", background_char);}); O("\n");}); 	//< draw down
+	DO((_ter_conf->ws_row - HEART_H)/2 + dif, {DO(_ter_conf->ws_col, {O("%c", heart_par->ch);}); O("\n");}); 	//< draw down
+	// DO((_ter_conf->ws_row - HEART_H)/2 + dif - 3, {DO(_ter_conf->ws_col, {O("%c", heart_par->ch);}); O("\n");}); 	//< draw down
+
 	fflush(stdout);
 }
+
+
+V window_dif_change(background heart_par, ter_conf _ter_conf, I pause)
+{
+	struct winsize a;
+	ter_conf _ter_var = &a;
+	I j, i, p = SEC/60;
+	j = pause/p;
+
+	show_img(heart_par, _ter_conf);
+
+	for (i = 0; i < j; i++) {
+		usleep(p);
+		ioctl(STDOUT_FILENO, TIOCGWINSZ, _ter_var);
+		if (_ter_var->ws_col != _ter_conf->ws_col || _ter_var->ws_row != _ter_conf->ws_row)	
+			show_img(heart_par, _ter_conf);
+	}
+}
+
+
 
 V draw_heart()																								//< print rand img
 {
 	FILE *db, *idx;
-	I pos, i, j, pause = SEC, n, bytesWaiting;
-	S *heart;
+	I pos, i, j, pause = SEC, n, num = 0;
 	C c = ' ';
-	pParams para = {0, 0};
+	pParams para = {0};
 	params par = &para;
 	pBackground back_;
-	background back = &back_;
+	background heart_par = &back_;
 	struct winsize a;
 	ter_conf _ter_conf = &a;
 
@@ -97,30 +131,36 @@ V draw_heart()																								//< print rand img
 	idx = fopen("bin/idx.dat", "r+b");
 
 	get_params(idx, par);
+	O("IM IN DRAW!\n");
 
-	heart = malloc(SZ(S) * HEART_H);
-	for (i = 0; i < HEART_H; i++) 
-		heart[i] = malloc(SZ(C) * par->max_line);
+	//<		background heart_par 	-->		keeps img colour sequence
+	//<		NEED TO:
+	//<		* get random position
+	//<		* get img sequence into background heart_par
+	//<		* make S* heart 
+	//< 	* pass to window dif change
+
 
 	CLEAR;
+	for (;;) {
+		// O("!\n");
+		num = rand() % par->amount;
+		while (!num)
+			num = rand() % par->amount;
+		// num = (!num) ? 1 : num;
 
-	for (;;) { 														
-		pos = get_addr(rand() % par->amount, idx);					//< get address of img in db
-		get_heart(db, pos, heart);									//< get img pattern into heart
-		get_background(db, back, pos);								//< get background info into back
-
+		pos = get_addr(num, idx); 									//<!!	check get_addr()!!!! 
+		get_background(db, heart_par, pos);							//< get img pattern into heart_par		//<!!	check get_backgorund()!!!!
 		pause = (pause == (SEC * 6)/7) ? (SEC*1)/2 : (SEC * 6)/7;	//< set pause
-		window_dif_change(heart, back->col, back->ch, _ter_conf, pause);				
-
-    	if(kbhit())													//< break if any key pressed
-    		break;
+		// pause = SEC * 3 /2;
+		window_dif_change(heart_par, _ter_conf, pause);	
+		// O("%d\n", num);
+		if(kbhit())													//< break if any key pressed
+    		break;	
 	}
 
 	O("%s\n", CNRM);												//<	set back to normal
 
-	for (i = 0; i < HEART_H; i++) 
-		free(heart[i]); 
-	free(heart);
 
 	fclose(db);
 	fclose(idx);
