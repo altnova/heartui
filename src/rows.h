f4c539f9fc6b541d5d4b4bd7a0180eb8ecaa87d7
altnova <arina@lateralsummer.com>
2018-10-06 23:17:03 +0300

    numerated img row printing functions

---
+++ src/rows.h
@@ -0,0 +1,11 @@
+
+ext	V row_1(S back_col, C back_c, S heart_col, S norm);
+ext	V row_2(S heart_col, S blink, S blink_col, S norm);
+
+ext	V row_3(S heart_col, S norm);
+
+ext	V row_4(S back_col, C back_c, S heart_col, S string, S str_col, S norm);
+
+ext	V row_5(S back_col, C back_c, S heart_col, S norm);
+ext	V row_6(S back_col, C back_c, S heart_col, S norm);
+ext	V row_7(S back_col, C back_c, S heart_col, S norm);
