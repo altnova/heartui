132d4583669557904cd3823691de5fc23b847807
altnova <arina@lateralsummer.com>
2018-10-06 23:05:12 +0300

    [-]removed j[39]

--- src/col_palette.c
+++ src/col_palette.c
@@ -61,7 +61,7 @@ V draw_palette(S* colour, I len)
 	for (i = 0; i < len; i++) {
 
 		if (k < 4) {
-			O("%s'%s'%s\t ", colour[i], str, "\x1b[0;0m");
+			O("%s'%s'%s\t", colour[i], str, "\x1b[0;0m");
 			k++;
 		}
 		else {
@@ -92,6 +92,9 @@ I lines()
 				if (j > 47)
 					break;
 			}
+			if (i > 9 && j == 39)
+				j++;
+
 			j = (IN(48, j, 89))	?	90	:
 				(IN(10, j, 29))	?	30	:
 				(IN(5, j, 6))	?	7	: j;
@@ -104,19 +107,22 @@ I lines()
 	}
 	cnt++;
 */
-	cnt = 941;
+	cnt = 918;
 	R cnt;
 }
 
 S* make_palette()
 {
-	I i, j, cnt = 0, line = lines();
+	I i, j, cnt = 0, line = lines(), k = 0;
 	S* colour = malloc(SZ(S) * line);
-
+	C str[8] = ":: ::\0";
 	for (i = 0; i < line + 1; i++)
 		colour[i] = malloc(SZ(C) * 50);
 
 	colour[cnt] = col(0,8, colour[cnt]);					//<	add 0;8
+	// O("%d\t%s'%s'%s  [%d;%dm \t",cnt, colour[cnt], str, "\x1b[0;0m", i, j);
+	// k++;
+
 	cnt++;
 
 	for (i = 0; i < 98; i++) {
@@ -134,17 +140,27 @@ S* make_palette()
 					break;
 			}
 
-			j = (IN(48, j, 89))	?	90	:
-				(IN(10, j, 29))	?	30	:
-				(IN(5, j, 6))	?	7	: j;
 
-			if (j == 8)
-				j++;
+			j = (IN(48, j, 89))		?	90	:
+				(IN(10, j, 29))		?	30	:
+				(IN(5, j, 6))		?	7	: 
+				(i > 9 && j == 39)	? 	40	: 
+				(j == 8)			?	9	: j;
 
+	
 			colour[cnt] = col(i, j, colour[cnt]);
+			/*if (k < 2) {
+				O("%d\t%s'%s'%s  [%d;%dm \t",cnt, colour[cnt], str, "\x1b[0;0m", i, j);
+				k++;
+			}
+			else {
+				O("%d\t%s'%s'%s  [%d;%dm\n", cnt, colour[cnt], str, "\x1b[0;0m", i, j);
+				k = 0;
+			}*/
 			cnt++;
 		}
 	}
+	O("\ntotal: %d\n", cnt);
 	colour[cnt][0] = 0;
 	R colour;
 }
@@ -195,6 +211,7 @@ S* make_palette()
 	I i, j, cnt = 0, line = lines();
 	S* colour = malloc(SZ(S) * line);
 
+
 	for (i = 0; i < line + 1; i++)
 		colour[i] = malloc(SZ(C) * 50);
 
@@ -213,12 +230,13 @@ S* make_palette()
 			if (i > 89 && j > 89)
 				break;
 
-			if (j == 40 || j == 92)
-				j++;
+			// if (j == 40 || j == 92)
+				// j++;
 
-			j = (IN(49, j, 89))	? 90	:
-				(IN(10, j, 29))	? 30	:
-				(IN(5, j, 8))	? 9		: j;		
+			j = (IN(49, j, 89))			? 90	:
+				(IN(10, j, 29))			? 30	:
+				(IN(5, j, 8))			? 9		:
+				(j == 40 || j == 92)	? j+1 	: j;		
 
 			if (i == 30 && IN(2, j, 30))
 				j = 31;
@@ -246,3 +264,13 @@ S* make_palette()
 //<	n	\0
 
 
+/*
+I main()
+{
+	draw_palette(make_palette(), lines());
+	make_palette();
+	O("total by lines(): %d\n", lines());
+	R0;
+}
+
+*/
